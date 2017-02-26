9239ae6c18828b42f405ecf05b9b7815d0cf19da
Chris Mathers <xee@xemedia.com>
2017-02-26 22:12:54 +0000

    About 90% complete.

--- app/components/sidebar/sidebar.js
+++ app/components/sidebar/sidebar.js
@@ -4,14 +4,6 @@ angular.module('myApp.sidebar', [])
 
     .controller('sidebarCtrl', ['$scope', '$http',  function($scope, $http) {
 
-        myapp.filter('findobj', function () {
-            return function (dataobj, selected) {
-                if (!selected) return dataobj;
-                return dataobj.filter(function (cats) {
-                    return cats.title === selected;
-                });
-            };
-        });
     }])
 
     .directive('sidebarContent', function( $http, $location, $routeParams ) {
@@ -22,16 +14,11 @@ angular.module('myApp.sidebar', [])
             link: function(scope, element,attrs) {
 
                 // get categories
-                $http.get('/model/data.json').success(function(data) {
+/*                $http.get('/model/data.json').success(function(data) {
                     scope.categories = angular.fromJson(data.categories);
-                });
+                });*/
 
                 scope.cols = ["timestamp", "score"]
-
-                scope.goTo = function ( path, routeParams ) {
-                    console.log("sidebar routeParams " +routeParams);
-                    $location.path(path, routeParams);
-                }
 
                 scope.getClass = function (path) {
                     return ($location.path().substr(0, path.length) === path) ? 'selected' : '';
--- app/detail/detail.js
+++ app/detail/detail.js
@@ -3,22 +3,38 @@
 angular.module('myApp.detail', ['ngRoute'])
 
     .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/detail', {
-            templateUrl: 'detail/detail.html',
-            controller: 'detailCtrl'
+        $routeProvider.when('/detail/:articleId', {
+            controller: 'detailCtrl',
+            templateUrl: function(articleId){ console.log('params ' +articleId); return 'detail/detail.html' }
         });
     }])
 
     .controller('detailCtrl', ['$route', '$http', '$scope', '$routeParams', '$sce', function($route, $http, $scope, $routeParams, $sce) {
 
         $scope.$sce = $sce;
+        $scope.articleId = $routeParams.articleId;
 
-        var paramValue = $route.current.$$route.routeParams;
-        console.log("paramValue = " +paramValue);
-
+        // get articles
         $http.get('/model/data.json').success(function(data) {
             $scope.articles = angular.fromJson(data.articles);
         });
+
+        // get categories
+        $http.get('/model/data.json').success(function(data) {
+            $scope.categories = angular.fromJson(data.categories);
+        });
+
+        $scope.getArticleCategory = function (catId) {
+            var result = [];
+            for(var i = 0 ; i < $scope.categories.length ; i++){
+                if(catId === $scope.categories[i].id){
+                    console.log("title " + $scope.categories[i].title);
+                    break;
+                }
+            }
+            //console.log(result[0]);
+            return $scope.categories[i].title;
+        }
 
     }])
 
@@ -26,13 +42,19 @@ angular.module('myApp.detail', ['ngRoute'])
 
         return {
             restrict: 'AEC',
-            //templateUrl: '/components/sidebar/sidebar.html',
+            //template: '<h2 ng-class=\"facetCategory1\">Article Category: {{getCatName(article.category)}}</h2>',
             link: function(scope, element,attrs) {
-/*
-                scope.goTo = function ( path, routeParams ) {
-                    console.log("routeParams " +routeParams);
-                    $location.path(path, routeParams);
-                }*/
+                scope.getArticleCategory = function (catId) {
+                    var result = [];
+                    for(var i = 0 ; i < $scope.categories.length ; i++){
+                        if(catId === $scope.categories[i].catId){
+                            result.push($scope.categories[i]);
+                        }
+                    }
+                    console.log(parentId);
+                    return result;
+                }
+
             }
         };
     });
